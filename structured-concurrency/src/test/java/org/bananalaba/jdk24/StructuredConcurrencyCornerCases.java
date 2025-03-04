8f0d539b5093cb67d439fcc0b1472d0fa194576c
Judzin <judzin.baranovsky@gmail.com>
2025-03-04 19:53:22 +0300

    Add examples on error handling and misuse of structured concurrency.

---
+++ structured-concurrency/src/test/java/org/bananalaba/jdk24/StructuredConcurrencyCornerCases.java
@@ -0,0 +1,85 @@
+package org.bananalaba.jdk24;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
+import lombok.extern.slf4j.Slf4j;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class StructuredConcurrencyCornerCases {
+
+    @Test
+    void shouldNotAllowPeerSubTaskInteraction() {
+        assertThatThrownBy(this::runDependentTasks)
+            .isInstanceOf(RuntimeException.class)
+            .hasCauseExactlyInstanceOf(ExecutionException.class);
+    }
+
+    private void runDependentTasks() {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            var latch = new CountDownLatch(1);
+            var subTask1 = scope.fork(() -> {
+                latch.await();
+                log.info("subtask-1 working");
+
+                return "result 1";
+            });
+
+            var subTask2 = scope.fork(() -> {
+                var dependencyResult = subTask1.get();
+                latch.countDown();
+
+                return "enriched " + dependencyResult;
+            });
+
+            scope.join();
+            scope.throwIfFailed();
+
+            log.info(subTask2.get());
+        } catch (Exception e) {
+            log.warn("failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void shouldHandleSubTaskExceptionAndShutdown() {
+        assertThatThrownBy(this::runWithShutdownPolicy)
+            .isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("subtask-1 failed");
+    }
+
+    private void runWithShutdownPolicy() {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            var subTask1 = scope.fork(() -> {
+                Thread.sleep(1000);
+                throw new RuntimeException("subtask-1 failed");
+            });
+
+            var latch = new CountDownLatch(1);
+            var subTask2 = scope.fork(() -> {
+                try {
+                    latch.await();
+
+                    log.info("subtask-2 working");
+
+                    return "subtask-2 result";
+                } catch (InterruptedException e) {
+                    log.warn("subtask-2 interrupted", e);
+                    throw e;
+                }
+            });
+
+            scope.join();
+            scope.throwIfFailed();
+
+            log.info(subTask1.get() + " and " + subTask2.get());
+        } catch (Exception e) {
+            log.warn("failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
