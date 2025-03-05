5a5e5ca61d5c5017b09e3184fc8b4fd7c2c1224d
Judzin <judzin.baranovsky@gmail.com>
2025-03-05 07:57:33 +0300

    Get rid of the API removed in JDK 24.

--- structured-concurrency/src/main/java/org/bananalaba/jdk24/tasktree/TracingContext.java
+++ structured-concurrency/src/main/java/org/bananalaba/jdk24/tasktree/TracingContext.java
@@ -5,7 +5,7 @@ public class TracingContext {
     private static final ScopedValue<String> TRACE_ID = ScopedValue.newInstance();
 
     public static void trace(final String traceId, final Runnable runnable) {
-        ScopedValue.runWhere(TRACE_ID, traceId, runnable);
+        ScopedValue.where(TRACE_ID, traceId).run(runnable);
     }
 
     public static String getTraceId() {
