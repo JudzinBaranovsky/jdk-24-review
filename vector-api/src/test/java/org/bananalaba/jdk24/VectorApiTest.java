c378e08bb4c5abe92bc9cd278af610e4c611f0c9
Judzin <judzin.baranovsky@gmail.com>
2025-03-06 09:04:36 +0300

    Extend the vector API example.

--- vector-api/src/test/java/org/bananalaba/jdk24/VectorApiTest.java
+++ vector-api/src/test/java/org/bananalaba/jdk24/VectorApiTest.java
@@ -17,6 +17,8 @@ public class VectorApiTest {
         var output = new int[3];
 
         for (var i = 0; i < inputA.length; i += SPECIES.length()) {
+            // if the input is not a multiple of the vector length, we need to mask
+            // internally, this might mean padding the vector with zeros which is not very efficient
             var mask = SPECIES.indexInRange(i, inputA.length);
 
             var aVector = IntVector.fromArray(SPECIES, inputA, i, mask);
@@ -24,6 +26,32 @@ public class VectorApiTest {
 
             var cVector = aVector.add(bVector);
             cVector.intoArray(output, i, mask);
+        }
+
+        assertThat(output).isEqualTo(new int[] {5, 7, 9});
+    }
+
+    @Test
+    void shouldAddTwoVectorsFaster() {
+        var inputA = new int[] {1, 2, 3};
+        var inputB = new int[] {4, 5, 6};
+
+        var output = new int[3];
+
+        var i = 0;
+        // as an alternative to masking, it's possible to apply vectorisation
+        // only to the input part which ideally fits the SIMD vector lengths...
+        for (i = 0; i < SPECIES.loopBound(inputA.length); i += SPECIES.length()) {
+            var aVector = IntVector.fromArray(SPECIES, inputA, i);
+            var bVector = IntVector.fromArray(SPECIES, inputB, i);
+
+            var cVector = aVector.add(bVector);
+            cVector.intoArray(output, i);
+        }
+
+        // ... and then calculate the remainder using plain Java (JIT will likely unwind this loop)
+        for (; i < inputA.length; i++) {
+            output[i] = inputA[i] + inputB[i];
         }
 
         assertThat(output).isEqualTo(new int[] {5, 7, 9});
